03a7dd9ca4aa6710d6d39dcbcbadaf8b825d6051
Kishore S <127382197+Kishore-7279@users.noreply.github.com>
2023-09-11 00:08:40 +0530

    MUICard.js

--- components/MUICard.js
+++ components/MUICard.js
@@ -1,48 +1,39 @@
-import { useState } from "react"
-import CommentCard from "./CommentCard"
+import {useState} from "react";
+import CommentCard from "./CommentCard";
 
-function MUICard(){
-    const [cardData, setCardData] = useState([])
-    const [comment, setComment] = useState([])
-    const [show, setShow] = useState({isShow : false})
+function MIUICard() {
+  const [commentData, setCommentData] = useState([])
+  const [postData, setPostData] = useState([])
+  const [show, setShow] = useState({isShow : false})
 
-    fetch("https://jsonplaceholder.typicode.com/posts")
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((data) => data.json())
+        .then(result => setPostData(result))
 
-    .then(data=> data.json())
-    
-    .then(result=>setCardData(result))
-    
-    .catch(err=>console.log(err))
+    function handleShow(postId){
+      setShow({isShow : !show.isShow})
+      fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+      .then(data => data.json())
+      .then(result => setCommentData(result))
+    }
 
-    function handleClick(id){
-        setShow({isShow : !show})
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-            .then(data => data.json())
-            .then(result => {setComment(result)
-            })
-            .catch(err => console.log(err))
-
-
-
-            }
-            function handleRemove(title){
-                document.getElementById(title).style.display = "none";
-            }
-        return(
-            <div className="card">
-                {cardData.map(val =>{
-                 return <div className="card-body" key={val.id} id={val.title}>
-                        <h1>{val.id} - {val.title}</h1>
-                        <h4>{val.body}</h4>
-
-                    <CommentCard comment = {comment} handleClick = {()=>{handleClick(val.id)}}/>
-                    <button onClick={()=>{handleRemove(val.title)}}>Delete Post</button>
-                        </div>
-                })}
-            </div>
-        )
+    function handleDelete(title){
+        document.getElementById(title).style.display = "none"
+    }
+    return (
+        <div className="card">
+      {postData.map((val) => {
+        return <div className = "post-body" id={val.title} name={val.title} >
+          <h2>{val.id} - {val.title}</h2>
+          <h2>{val.body}</h2>
+          <button onClick = {()=>{handleShow(val.id)}}>Comment</button>
+          <button onClick={()=>{handleDelete(val.title)}}>Delete</button>
+        </div>
+      })}
+      {show.isShow ? <CommentCard handleShow = {handleShow} commentData = {commentData}/> : null}
+    </div>
+  );
+    }
 
 
-}
-
-export default MUICard
+export default MIUICard;
