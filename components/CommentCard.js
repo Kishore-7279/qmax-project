36c4d355b1a403f5dce862f1c1994b720c16df1c
Kishore S <127382197+Kishore-7279@users.noreply.github.com>
2023-09-11 00:06:36 +0530

    CommentCard.js

--- components/CommentCard.js
+++ components/CommentCard.js
@@ -1,17 +1,19 @@
-function CommentCard(props){
-    return(
-      
-       <div>
-           {props.comment.map(val =>{
-            return <div key={val.id}> 
-                <h6>{val.id} - {val.name}</h6>
-                <h6>{val.email}</h6>
-                <h6>{val.body}</h6>
-            </div>
-           })}
-       </div>
-    )
-}
+import ReactDOM from "react-dom"
 
+function CommentCard({commentData, handleShow}){
+    return ReactDOM.createPortal(
+    <div className = 'comment-card'>
+    {commentData.map(val =>{
+      return <div className = "comment-body">
+        <h2>{val.id} - {val.name}</h2>
+        <h2>{val.email}</h2>
+        <h2> {val.body}</h2>
+      </div>
+    })}
+    <button onClick = {()=>{handleShow()}}>Hide Comments</button>
+    </div>,
+    document.getElementById('portal')
+  )
+}
 
 export default CommentCard
